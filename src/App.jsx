81f864b5d028ae02b190ff190e965e34878142c7
Shubhjeet <shubhjeet.calphawizz@gmail.com>
2025-12-11 19:40:12 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./Pages/About";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { Route, Routes } from "react-router-dom";
+import ServiceDetails from "./Pages/ServiceDetails";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<About />} path="/about" />
+          <Route element={<ServiceDetails />} path="/service-details" />
         </Routes>
         <Footer />
       </div>
